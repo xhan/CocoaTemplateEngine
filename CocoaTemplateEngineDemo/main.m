08f15f8adf4fd98d32ffdbe5176e2d04301b04f2
xhan <xhan87@gmail.com>
2013-08-07 19:21:05 +0800

    init proj

---
+++ CocoaTemplateEngineDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CocoaTemplateEngineDemo
+//
+//  Created by xhan on 8/7/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
